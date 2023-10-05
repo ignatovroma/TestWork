a9af0364202a041e6f8a60d38e3bbb7054ac3048
IgnatovRoman <oneok@bk.ru>
2023-10-06 00:24:19 +0300

    create Promt

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿string[] PromtArray(string message)
+{
+    Console.Clear();
+    Console.Write(message);
+    string[] promt = Console.ReadLine().Split(' ');
+    Console.Clear();
+    return promt;
+}
+
+int Promt(string message)
+{
+    Console.Clear();
+    Console.Write(message);
+    var value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    Console.Clear();
+    return result;
+}
+
+string[] inputArray = PromtArray("Введите элементы массива через пробел: ");
+int elementLength = Promt("Введите количество знаков элементов нового массива: ");
