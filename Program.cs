213f0614b366fb1d00560fc6fa2f0af91692ffd2
IgnatovRoman <oneok@bk.ru>
2023-10-06 23:14:19 +0300

    create counter &  output array

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@
     Console.Clear();
     Console.Write(message);
     string[] promt = Console.ReadLine().Split(' ');
-    Console.Clear();
     return promt;
 }
 
@@ -17,5 +16,39 @@ int Promt(string message)
     return result;
 }
 
+int ArrayElementCouter(string[] inputArr, int elLength)
+{
+    int count = 0;
+    for (int i = 0; i < inputArr.Length; i++)
+    {
+        inputArr[i] = inputArr[i].Trim('"');
+        if (inputArr[i].Length <= elLength) count++;
+    }
+    return count;
+}
+
 string[] inputArray = PromtArray("Введите элементы массива через пробел: ");
 int elementLength = Promt("Введите количество знаков элементов нового массива: ");
+int count = ArrayElementCouter(inputArray, elementLength);
+
+string[] NewArray(int count, int elLength)
+{
+    string[] outputArray = new string[count];
+    int index = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= elLength)
+        {
+            outputArray[index] = inputArray[i];
+            index++;
+        }
+    }
+    return outputArray;
+}
+
+string[] printArray = NewArray(count, elementLength);
+Console.WriteLine("Новый массив:");
+for (int i = 0; i < printArray.Length; i++)
+{       
+    Console.Write('\u0022' + printArray[i] + '\u0022' + " ");    
+}
