7784393dad6b977ad9888527b7d66a43d40b23bb
IgnatovRoman <oneok@bk.ru>
2023-10-10 23:29:39 +0300

    print array v2

--- Program.cs
+++ Program.cs
@@ -1,76 +1,10 @@
-﻿// string[] PromtArray(string message)
-// {
-//     Console.Clear();
-//     Console.Write(message);
-//     string[] promt = Console.ReadLine().Split(' ');
-//     return promt;
-// }
-
-// int Promt(string message)
-// {
-//     Console.Clear();
-//     Console.Write(message);
-//     var value = Console.ReadLine();
-//     int result = Convert.ToInt32(value);
-//     Console.Clear();
-//     return result;
-// }
-
-// int ArrayElementCouter(string[] inputArr, int elLength)
-// {
-//     int count = 0;
-//     for (int i = 0; i < inputArr.Length; i++)
-//     {
-//         inputArr[i] = inputArr[i].Trim('"');
-//         if (inputArr[i].Length <= elLength) count++;
-//     }
-//     return count;
-// }
-
-// string[] inputArray = PromtArray("Введите элементы массива через пробел: ");
-// int elementLength = Promt("Введите количество знаков элементов нового массива: ");
-// int count = ArrayElementCouter(inputArray, elementLength);
-
-// string[] NewArray(int count, int elLength)
-// {
-//     string[] outputArray = new string[count];
-//     int index = 0;
-//     for (int i = 0; i < inputArray.Length; i++)
-//     {
-//         if (inputArray[i].Length <= elLength)
-//         {
-//             outputArray[index] = inputArray[i];
-//             index++;
-//         }
-//     }
-//     return outputArray;
-// }
-
-// string[] printArray = NewArray(count, elementLength);
-// // Console.Write("Исходный массив: ");
-// // for (int i = 0; i < inputArray.Length; i++)
-// // {       
-// //     Console.Write(inputArray[i] + " ");    
-// // }
-// Console.WriteLine();
-// Console.Write("Новый массив:    ");
-// for (int i = 0; i < printArray.Length; i++)
-// {       
-//     Console.Write('\u0022' + printArray[i] + '\u0022' + " ");    
-// }
-
-static string[] PromtArray(string message)
+﻿string[] PromtArray(string message)
 {
     Console.Clear();
     Console.Write(message);
     string[] promtArray = Console.ReadLine().Split(' ');
     Console.Clear();
-    Console.Write("Исходный массив: ");
-    foreach (string arr in promtArray)
-    {
-        Console.Write(arr + " ");
-    }
-    Console.WriteLine();
+
     return promtArray;
 }
 
@@ -79,24 +13,28 @@ string[] inputArray = PromtArray("Введите элементы массива
 string[] OutputArray(string[] array)
 {
     int count = 0;
-    for (int i = 0; i < inputArray.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if (inputArray[i].Length <= 3)
+        if (array[i].Length <= 3)
         {
-            inputArray[count] = inputArray[i];
+            array[count] = array[i];
             count++;
         }
     }
-    Array.Resize(ref inputArray, count);
-    return inputArray;
+    Array.Resize(ref array, count);
+    return array;
 }
 
-OutputArray(inputArray);
-
-Console.Write("Новый массив:    ");
-foreach (string arr in inputArray)
+void PrintArray(string message, string[] array)
 {
-    Console.Write(arr + " ");
+    Console.Write(message + " ");
+    foreach (string arr in array)
+    {
+        Console.Write(arr + " ");
+    }
+    Console.WriteLine();
 }
+PrintArray("Исходный массив:", inputArray);
+PrintArray("Новый массив:   ", OutputArray(inputArray));
 
 Console.ReadKey();
